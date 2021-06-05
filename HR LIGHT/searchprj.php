e0d951e3f42957a851b67aa8b5ba82325e39ef7a
THATAVARTHI AKASH <68628036+akash14650@users.noreply.github.com>
2021-06-05 23:07:34 +0530

    Delete HR LIGHT directory

--- HR LIGHT/searchprj.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include 'connection.php';
-    if(isset($_POST['Search'])){
-        $projname=$_POST['projname'];
-        $selectsql="SELECT * FROM `project_details` WHERE proj_name LIKE '%$projname%'";
-    }
-?>
